e4b1d7751eb1eee996692d979274291b5ccd06da
carlosh-07 <95hernandezcarlos@gmail.com>
2023-06-15 09:21:01 -0400

    FLoat text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
+      <h2 className={styles.floatText}>Real-time Departures</h2>
       <div className={styles.main}>
         <a className={styles.fullLinkLeft} href="/route">
           By Route
